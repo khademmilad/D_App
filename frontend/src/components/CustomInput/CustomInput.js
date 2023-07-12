fcd10516c3cf826efe3e0ff86f8aed39b9984335
khademmilad <khademmilad@gmail.com>
2023-07-12 16:45:05 +0200

    design UI Button in landing page

--- frontend/src/components/CustomInput/CustomInput.js
+++ frontend/src/components/CustomInput/CustomInput.js
@@ -1,10 +1,16 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
-const CustomInput = () => {
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
   return (
     <View style={styles.container}>
-      <TextInput placeholder='placeholder' style={styles.input} />
+      <TextInput 
+      value={value}
+      onChangeText={setValue}
+      placeholder={placeholder}
+      style={styles.input} 
+      secureTextEntry={secureTextEntry}
+      />
     </View>
   )
 }
--- frontend/src/screens/Login/Login.js
+++ frontend/src/screens/Login/Login.js
@@ -1,16 +1,47 @@
 import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native'
-import React from 'react'
-import Logo from '../../../assets/images/logo.jpg'
+import React, {useState} from 'react'
+import Logo from '../../../assets/images/logo.png'
 import CustomInput from '../../components/CustomInput/CustomInput'
+import CustomButton from '../../components/CustomButton/CustomButton'
+
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const {height} = useWindowDimensions();
+
+    const onSignInPressed = () => {
+        console.warn("sign in")
+    }
+
+    const onForgotPasswordPressed = () => {
+        console.warn("Forgot password")
+    }
   return (
     <View style={styles.root}>
       <Image source={Logo} style={[styles.logo, {height: height * 0.3}]} resizeMode='contain' />
 
-      <CustomInput />
-      <CustomInput />
+      <CustomInput 
+        placeholder="Username" 
+        value={username} 
+        setValue={setUsername}
+      />
+      <CustomInput 
+        placeholder="Password" 
+        value={password} 
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
+      <CustomButton
+        text="Sign In"
+        onPress={onSignInPressed}
+      />
+      <CustomButton
+        text="Forgot Password?"
+        onPress={onForgotPasswordPressed}
+        type="TERTIARY"
+      />
     </View>
   )
 }
@@ -21,9 +52,9 @@ const styles = StyleSheet.create({
         padding: 20,
     },
     logo: {
-        width: 200,
-        maxWidth: 200,
-        height: 200,
+        width: 250,
+        maxWidth: 250,
+        height: 250,
     }
 })
 
