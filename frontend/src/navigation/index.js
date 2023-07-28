5c6d2d208892a1fa3768facc0432db7e38a8a477
khademmilad <khademmilad@gmail.com>
2023-07-28 17:19:00 +0200

    added all screens to navigator

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -3,6 +3,11 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/Login/Login'
+import SignUp from '../screens/SignUp/SignUp'
+import NewPassword from '../screens/NewPassword/NewPassword'
+import ConfirmEmail from '../screens/ConfirmEmail/ConfirmEmail'
+import ForgotPassword from '../screens/ForgotPassword/ForgotPassword'
+
 
 // Define the Stack variable
 const Stack = createStackNavigator();
@@ -12,6 +17,10 @@ const Navigation = () => {
     <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name="SignIn" component={Login} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="NewPassword" component={NewPassword} />
+            <Stack.Screen name="ConfirmEmail" component={ConfirmEmail} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         </Stack.Navigator>
     </NavigationContainer>
   )
