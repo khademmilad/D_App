901bc25ba9016a781031043d7bc8bf74d87d9b05
khademmilad <khademmilad@gmail.com>
2023-07-28 16:49:42 +0200

    created Login navigator

--- frontend/src/navigation/index.js
+++ frontend/src/navigation/index.js
@@ -1,11 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../screens/Login/Login'
+
+// Define the Stack variable
+const Stack = createStackNavigator();
 
 const Navigation = () => {
   return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SignIn" component={Login} />
+        </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
