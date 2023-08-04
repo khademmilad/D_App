32ee41386ab6aeae3e7c1a62f4a9b9feb75af789
khademmilad <khademmilad@gmail.com>
2023-08-04 23:42:08 +0200

    responsive the card component

--- frontend/src/screens/Swipe/Swipe.js
+++ frontend/src/screens/Swipe/Swipe.js
@@ -1,17 +1,22 @@
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Dimensions } from 'react-native'
 import React from 'react'
 import ImageSwipe from '../../../assets/images/swipe.jpg'
 
+const { width, height } = Dimensions.get('window');
 
 const Swipe = () => {
   return (
     <View style={styles.root}>
       <View style={styles.card}>
-        <ImageBackground source={ImageSwipe} style={[styles.image]} resizeMode='contain'>
-          <Text style={styles.name}>Elon Musk</Text>
-          <Text style={styles.bio}>Elon Musk</Text>
+        <ImageBackground source={ImageSwipe} style={styles.image} resizeMode='cover'>
+          <View style={styles.cartInner} >
+            <View style={styles.textContainer}>
+              <Text style={styles.name}>Elon Musk</Text>
+              <Text style={styles.bio}>Elon Musk's biography goes here...</Text>
+            </View>
+          </View>
         </ImageBackground>
-      </View> 
+      </View>
     </View>
   )
 }
@@ -20,12 +25,13 @@ const styles = StyleSheet.create({
   root: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
   },
   card: {
-    width: '83%',
-    height: '70%',
+    width: width * 0.9,
+    height: height * 0.6,
     borderRadius: 10,
+    overflow: 'hidden',
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -33,22 +39,29 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.37,
     shadowRadius: 7.49,
-
     elevation: 12,
   },
+  cartInner:{
+    padding: 10,
+    backgroundColor: 'red'
+  },
   image: {
+    flex: 1,
     width: '100%',
     height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden'
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 16,
   },
   name: {
-    fontSize: 30,
+    fontSize: 0.05 * width, // Adjust font size based on screen width
     color: 'white',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   bio: {
-    fontSize: 18,
+    fontSize: 0.04 * width, // Adjust font size based on screen width
     color: 'white',
     lineHeight: 24,
   }
