6e36c7e2f9a6a1982a78bc25e1fb1da6287eee12
khademmilad <khademmilad@gmail.com>
2023-08-05 14:39:35 +0200

    added onpress btn to move card

--- frontend/src/screens/Swipe/Swipe.js
+++ frontend/src/screens/Swipe/Swipe.js
@@ -1,28 +1,36 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 import SwipeCard from '../../components/SwipeCard/SwipeCard';
 import ImageSwipe from '../../../assets/images/swipe.jpg';
-
-const { width, height } = Dimensions.get('window');
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 
 const Swipe = () => {
+  const sharedValue = useSharedValue(1); // Starting opacity value
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [{
+      translateX: sharedValue.value * 100,
+    },
+    ],
+  }));
+
+  const changeOpacity = () => {
+    sharedValue.value = Math.random();
+  };
+
   return (
     <View style={styles.root}>
-      <SwipeCard
-        name="Elon Musk"
-        bio="Elon Musk's biography goes here..."
-        imageSource={ImageSwipe}
-      />
-      <SwipeCard
-        name="Elon Musk"
-        bio="Elon Musk's biography goes here..."
-        imageSource={ImageSwipe}
-      />
-      <SwipeCard
-        name="Elon Musk"
-        bio="Elon Musk's biography goes here..."
-        imageSource={ImageSwipe}
-      />
+      <Animated.View style={[styles.animatedCard, cardStyle]}>
+        <SwipeCard
+          name="Elon Musk"
+          bio="Elon Musk's biography goes here..."
+          imageSource={ImageSwipe}
+        />
+      </Animated.View>
+      <Pressable onPress={changeOpacity}>
+        <Text>Change value</Text>
+      </Pressable>
     </View>
   );
 };
@@ -31,7 +39,13 @@ const styles = StyleSheet.create({
   root: {
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
+  },
+  animatedCard: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
