5863e90742f7b6e408971175abe07cda33dc8643
khademmilad <khademmilad@gmail.com>
2023-08-07 13:58:50 +0200

    useAnimatedGestureHandler

--- frontend/src/screens/Swipe/Swipe.js
+++ frontend/src/screens/Swipe/Swipe.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { View, StyleSheet, Text, Pressable } from 'react-native';
 import SwipeCard from '../../components/SwipeCard/SwipeCard';
 import ImageSwipe from '../../../assets/images/swipe.jpg';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler } from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
 
 const Swipe = () => {
   const sharedValue = useSharedValue(1); // Starting opacity value
@@ -19,15 +21,29 @@ const Swipe = () => {
     sharedValue.value = Math.random();
   };
 
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_) => {
+      console.warn('Touch start')
+    },
+    onActive: (event) => {
+      console.log('Touch x', event.translationX)
+    },
+    onEnd: () => {
+      console.warn('Touch ended')
+    }
+  })
+
   return (
     <View style={styles.root}>
-      <Animated.View style={[styles.animatedCard, cardStyle]}>
-        <SwipeCard
-          name="Elon Musk"
-          bio="Elon Musk's biography goes here..."
-          imageSource={ImageSwipe}
-        />
-      </Animated.View>
+      <PanGestureHandler gestureHandler={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+          <SwipeCard
+            name="Elon Musk"
+            bio="Elon Musk's biography goes here..."
+            imageSource={ImageSwipe}
+          />
+        </Animated.View>
+      </PanGestureHandler>
       <Pressable onPress={() => (sharedValue.value = withSpring(Math.random()))}>
         <Text>Change value</Text>
       </Pressable>
